734e43f669856a9d5f3d051597ad0f6273a35ec2
max197a <max197aa@gmail.com>
2022-11-07 13:13:27 +0200

    add form

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,39 @@ $(document).ready(function() {
         $('[js-hamburger]').removeClass("is-open");
     });
 
+    (function($) {
+        'use strict';
+        var form = $('.contact__form'),
+            message = $('.contact__msg'),
+            form_data;
+        // Success function
+        function done_func(response) {
+            $('.success__msg').fadeIn();
+            setTimeout(function() {
+                message.fadeOut();
+            }, 2000);
+            form.find('input:not([type="submit"]), textarea').val('');
+        }
+        // fail function
+        function fail_func(data) {
+            $('.fail__msg').fadeIn();
+            setTimeout(function() {
+                message.fadeOut();
+            }, 2000);
+        }
+
+        form.submit(function(e) {
+            e.preventDefault();
+            form_data = $(this).serialize();
+            $.ajax({
+                    type: 'POST',
+                    url: form.attr('action'),
+                    data: form_data
+                })
+                .done(done_func)
+                .fail(fail_func);
+        });
+
+    })(jQuery);
+
 });
